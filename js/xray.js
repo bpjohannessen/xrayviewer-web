4cd840ca1f17f0fe15c3a1a74f2093556c510fd4
Bjørn-Petter Johannessen <bpjohannessen@users.noreply.github.com>
2023-02-02 21:25:00 +0100

    Added hide function for diagnosis; removed some php code

--- js/xray.js
+++ js/xray.js
@@ -18,29 +18,61 @@ $(document).ready(function() {
 			}
 		});
 	});
+	
 
 		var $queryurl = "http://localhost:3000/api";
 		console.log($queryurl);
 
 		$.getJSON($queryurl, function (json) {
 			var $main = $("div#main");
+			var $table = $("table#tableMain")
+
 
 			console.log("Printing json");
 			//console.log(json);
 
 			$.each(json, function (key, value) {
-				console.log("pikken 1:" + value._desc);
-				$main.append("<h5>" + key + " - " +	value._desc + "</h5>");
+				$table.append("<thead id='thead"+key+"'><tr><th colspan='2' id='th"+key+"'>");
+				var $th = $("th#th"+key);
+				// console.log("pikken 1:" + value._desc);
+				$th.append("<h5><a href='#' onclick='$(hiddenText" + key +").show(); return false;'>#"+key+"</a><span style='display: none;' id='hiddenText"+key+"'> "+value._desc+"</span>");
+				// + key + " - " +	value._desc + "</h5>");
+				//$header = "<a href='#' onclick='$(hiddenText".$key.").show(); return false;'>#".$key."</a><span style=\"display: none;\" id=\"hiddenText".$key."\"> - ".$fr."</span>";
+
+				$table.append("</th></tr></thead>");
+				$table.append("<tbody class='popup-gallery' id='gallery"+key+"'><tr>")
+				$gallerybody = $("tbody#gallery"+key);
+
+				var noImages = value._images.length;
+				var noLeft;
+				console.log("Number:" + noImages);
+
+
 				$.each(value._images, function(id, image) {
+
+					//$filename = $src.$folder."/".$file;
+					var filename = "./src/ortho/"+key + "/" + image;
+
+					console.log(filename);
+
 					console.log(id);
 					console.log(image);
-					$main.append("<img src='./src/ortho/"+key + "/" +image+"' style='width: 100%; height: auto;'>");
+					$gallerybody.append("<td><a href="+filename+" title='FETTE NAJSs'><img src='./src/ortho/"+key + "/" +image+"' style='width: 100%; height: auto;'>");
+					noLeft = noImages - 1;
+					if(noLeft % 2 == 0) {
+						$gallerybody.append("</td></tr>");
+						if(noLeft != 0) $gallerybody.append("<tr>");
+
+					} else {
+						$gallerybody.append("</td>");
+					}
+
+					//$gallerybody.append(id + " -kukken " + image);
+
+					//$main.append("<img src='./src/ortho/"+key + "/" +image+"' style='width: 100%; height: auto;'>");
 				});
+				$table.append("</tbody>");
+
 			});
-
-
-
 		});
-
-});
-
+	});
